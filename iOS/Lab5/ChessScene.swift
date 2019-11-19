87331fa165b993c8983c6d2bd702935148f245b6
Will Lacey <WillLacey@Will-Laceys-MacBook-Pro-4.local>
2019-11-19 11:40:16 -0600

    Uodated iOS; Removed Jupyter Cache

--- iOS/Lab5/ChessScene.swift
+++ iOS/Lab5/ChessScene.swift
@@ -18,25 +18,30 @@ class ChessScene: SKScene {
     let boardWidth = CGFloat(1.0)
     let boardHeight = CGFloat(0.575)
     
-    let test = SKSpriteNode(imageNamed: "black_pawn.png")
-    lazy var gridPoints = generateGridPoints()
+    var selectedSquare: ChessSquare?
     
-    var squares = [[ChessSquare]]()
+    lazy var chessBoard = generateChessSquares()
     
     // MARK: Scene Functions
     override func didMove(to view: SKView) {
         addBoard()
         setBoard()
-        test.zPosition = 2
-        self.addChild(test)
     }
     
-    func generateGridPoints()  -> [[(Double, Double)]] {
-        
-        var gridPoints = [[(Double, Double)]]()
+    func addBoard() {
+        let board = SKSpriteNode(imageNamed: "Board1.png")
+        board.size = CGSize(width: size.width * self.boardWidth, height: size.height * self.boardHeight)
+        board.position = CGPoint(x: size.width*0.5, y: size.height*0.5)
+        board.isUserInteractionEnabled = false
+        board.zPosition = 0
+        self.addChild(board)
+    }
+    
+    func generateChessSquares() -> [[(ChessSquare)]] {
+        var chessBoard = [[ChessSquare]]()
         
         for row in 0...7 {
-            var gridRow = [(Double, Double)]()
+            var chessRow = [ChessSquare]()
             for col in 0...7 {
                 
                 let yBoardOffset = 0.5 / 2
@@ -44,90 +49,147 @@ class ChessScene: SKScene {
                 let ySquareOffset = Double(self.boardHeight) / 8
                 
                 let xPos = (xSquareOffset/2) + (xSquareOffset) * Double(col)
-                let yPos = (yBoardOffset) + (ySquareOffset) * Double(row)
-                let point = (xPos, yPos)
-                gridRow.append(point)
+                let yPos = (yBoardOffset) + (ySquareOffset) * Double(7 - row)
+                
+                let square = ChessSquare.init(arrayRow: row, arrayCol: col, spriteScenePosition: CGPoint.init(x: xPos, y: yPos), xSquareOffset: CGFloat(xSquareOffset), ySquareOffset: CGFloat(ySquareOffset), inChessScene: self)
+                chessRow.append(square)
             }
-            gridPoints.append(gridRow)
+            chessBoard.append(chessRow)
         }
-        return gridPoints
-    }
-    
-    func addBoard() {
-        let board = SKSpriteNode(imageNamed: "Board1.png")
-        board.size = CGSize(width: size.width * self.boardWidth, height: size.height * self.boardHeight)
-        board.position = CGPoint(x: size.width*0.5, y: size.height*0.5)
-        board.zPosition = 1
-        self.addChild(board)
+        return chessBoard
     }
     
     func setBoard() {
         // Pawns
         for i in 0...7 {
-            setPiece(pieceName: "black_pawn.png", pieceSquare: self.gridPoints[6][i])
+            self.chessBoard[1][i].setPiece(pieceSpriteFileName: "black_pawn")
         }
         for i in 0...7 {
-            setPiece(pieceName: "white_pawn.png", pieceSquare: self.gridPoints[1][i])
+            self.chessBoard[6][i].setPiece(pieceSpriteFileName: "white_pawn")
         }
-        
+
         // Rooks
-        setPiece(pieceName: "black_rook.png", pieceSquare: self.gridPoints[7][0])
-        setPiece(pieceName: "black_rook.png", pieceSquare: self.gridPoints[7][7])
-        setPiece(pieceName: "white_rook.png", pieceSquare: self.gridPoints[0][0])
-        setPiece(pieceName: "white_rook.png", pieceSquare: self.gridPoints[0][7])
-        
+        self.chessBoard[0][0].setPiece(pieceSpriteFileName: "black_rook")
+        self.chessBoard[0][7].setPiece(pieceSpriteFileName: "black_rook")
+        self.chessBoard[7][0].setPiece(pieceSpriteFileName: "white_rook")
+        self.chessBoard[7][7].setPiece(pieceSpriteFileName: "white_rook")
+
         // Knights
-        setPiece(pieceName: "black_knight.png", pieceSquare: self.gridPoints[7][1])
-        setPiece(pieceName: "black_knight.png", pieceSquare: self.gridPoints[7][6])
-        setPiece(pieceName: "white_knight.png", pieceSquare: self.gridPoints[0][1])
-        setPiece(pieceName: "white_knight.png", pieceSquare: self.gridPoints[0][6])
-        
+        self.chessBoard[0][1].setPiece(pieceSpriteFileName: "black_knight")
+        self.chessBoard[0][6].setPiece(pieceSpriteFileName: "black_knight")
+        self.chessBoard[7][1].setPiece(pieceSpriteFileName: "white_knight")
+        self.chessBoard[7][6].setPiece(pieceSpriteFileName: "white_knight")
+
         // Bishops
-        setPiece(pieceName: "black_bishop.png", pieceSquare: self.gridPoints[7][2])
-        setPiece(pieceName: "black_bishop.png", pieceSquare: self.gridPoints[7][5])
-        setPiece(pieceName: "white_bishop.png", pieceSquare: self.gridPoints[0][2])
-        setPiece(pieceName: "white_bishop.png", pieceSquare: self.gridPoints[0][5])
-        
+        self.chessBoard[0][2].setPiece(pieceSpriteFileName: "black_bishop")
+        self.chessBoard[0][5].setPiece(pieceSpriteFileName: "black_bishop")
+        self.chessBoard[7][2].setPiece(pieceSpriteFileName: "white_bishop")
+        self.chessBoard[7][5].setPiece(pieceSpriteFileName: "white_bishop")
+
         // Queens
-        setPiece(pieceName: "black_queen.png", pieceSquare: self.gridPoints[7][3])
-        setPiece(pieceName: "white_queen.png", pieceSquare: self.gridPoints[0][3])
+        self.chessBoard[0][3].setPiece(pieceSpriteFileName: "black_queen")
+        self.chessBoard[7][3].setPiece(pieceSpriteFileName: "white_queen")
 
         // Kings
-        setPiece(pieceName: "black_king.png", pieceSquare: self.gridPoints[7][4])
-        setPiece(pieceName: "white_king.png", pieceSquare: self.gridPoints[0][4])
+        self.chessBoard[0][4].setPiece(pieceSpriteFileName: "black_king")
+        self.chessBoard[7][4].setPiece(pieceSpriteFileName: "white_king")
     }
     
-    func setPiece(pieceName: String, pieceSquare: (Double, Double)) {
-        let piece = SKSpriteNode(imageNamed: pieceName)
-        piece.size = CGSize(width: size.width * self.pieceWidth, height: size.height * self.pieceHeight)
-        piece.position = CGPoint(x: size.width * CGFloat(pieceSquare.0), y: size.height * CGFloat(pieceSquare.1))
-        piece.zPosition = 2
-        self.addChild(piece)
+    func selectSquare(position: CGPoint) -> ChessSquare {
+        
+        var selectedRow = 0
+        var selectedCol = 0
+        
+        for r in 0...7 {
+            if position.y < self.chessBoard[r][0].getSquareTopBoundsPosition() {
+                selectedRow = r
+            }
+        }
+            
+        for c in 0...7 {
+            if position.x > self.chessBoard[0][c].getSquareLeftBoundsPosition() {
+                selectedCol = c
+            }
+        }
+        
+        return self.chessBoard[selectedRow][selectedCol]
+    }
+    
+    // Mark: Getters and Setters
+    func getPieceWidth() -> CGFloat {
+        return self.pieceWidth
+    }
+    
+    func getPieceHeight() -> CGFloat {
+        return self.pieceHeight
     }
     
     // MARK: Touch Functions
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        let touch = touches.first!
-        let touchLocation = touch.location(in: self.view)
-        test.size = CGSize(width: 2 * size.width * self.pieceWidth, height: 2 * size.height * self.pieceHeight)
-        test.position = CGPoint(x: touchLocation.x, y: size.height - touchLocation.y + 40)
-        test.zPosition = 3
+        
+        let touch:UITouch = touches.first!
+        let touchPosition = touch.location(in: self)
+        let xScenePos =  touchPosition.x / size.width
+        let yScenePos = touchPosition.y / size.height
+        let scenePosition = CGPoint(x: xScenePos, y: yScenePos)
+        
+        if self.selectedSquare != nil {
+            
+            let newSelectedSquare = self.selectSquare(position: scenePosition)
+            let validMove = true // Todo: This has got to change bro
+            
+            if validMove {
+                self.selectedSquare!.removePiece()
+                if newSelectedSquare.hasPiece {
+                    newSelectedSquare.removePiece()
+                }
+                newSelectedSquare.setPiece(pieceSpriteFileName: self.selectedSquare!.pieceSpriteFileName)
+            }
+            
+            self.selectedSquare = nil
+            
+        } else {
+            
+            self.selectedSquare = self.selectSquare(position: scenePosition)
+            if self.selectedSquare!.hasPiece {
+                // get_valid_moves(color, row, col)
+                // if valid moves
+                    // highlight potential squares
+                    // make move
+                    // update game
+                
+//                var availableCoordinates = [(Int, Int)]()
+//                availableCoordinates.append((self.selectedSquare!.row, self.selectedSquare!.col))
+//                availableCoordinates.append((5,4))
+//                availableCoordinates.append((4,4))
+//                availableCoordinates.append((3,3))
+//                for coordinate in availableCoordinates {
+//                    let row = coordinate.0
+//                    let col = coordinate.1
+//                    self.chessBoard[row][col].isHighlightedSquare = true
+//                }
+                
+            } else {
+                self.selectedSquare = nil
+            }
+        }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        let touch = touches.first!
-        let touchLocation = touch.location(in: self.view)
-        test.size = CGSize(width: 2 * size.width * self.pieceWidth, height: 2 * size.height * self.pieceHeight)
-        test.position = CGPoint(x: touchLocation.x, y: size.height - touchLocation.y + 40)
-        test.zPosition = 3
+//        let touch = touches.first!
+//        let touchLocation = touch.location(in: self.view)
+//        test.size = CGSize(width: 2 * size.width * self.pieceWidth, height: 2 * size.height * self.pieceHeight)
+//        test.position = CGPoint(x: touchLocation.x, y: size.height - touchLocation.y + 40)
+//        test.zPosition = 3
+//        NSLog("\(test.position)")
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        let touch = touches.first!
-        let touchLocation = touch.location(in: self.view)
-        test.size = CGSize(width: size.width * self.pieceWidth, height: size.height * self.pieceHeight)
-        test.position = CGPoint(x: touchLocation.x, y: size.height - touchLocation.y + 20)
-        test.zPosition = 2
+//        let touch = touches.first!
+//        let touchLocation = touch.location(in: self.view)
+//        test.size = CGSize(width: size.width * self.pieceWidth, height: size.height * self.pieceHeight)
+//        test.position = CGPoint(x: touchLocation.x, y: size.height - touchLocation.y + 20)
+//        test.zPosition = 2
     }
     
 }
--- iOS/Lab5/ChessSquare.swift
+++ iOS/Lab5/ChessSquare.swift
@@ -7,11 +7,90 @@
 //
 
 import UIKit
+import SpriteKit
 
 class ChessSquare: NSObject {
-    // position
-    // sqare name
-    // piece
-    // light up square
-    // 
+    
+    var spriteScenePosition = CGPoint(x:0, y:0)
+    var xSquareOffset = CGFloat(0)
+    var ySquareOffset = CGFloat(0)
+    var row: Int
+    var col: Int
+    var hasPiece = false
+    var chessScene: ChessScene
+    
+    // Initialize with a placeholder value
+    var pieceSpriteFileName = "white_pawn"
+    var pieceSprite = SKSpriteNode(imageNamed: "white_pawn")
+    
+    var highlightSpriteFileName = "black_pawn"
+    var highlightSprite = SKSpriteNode(imageNamed: "black_pawn")
+    var isHighlightedSquare = false {
+        didSet{
+            if isHighlightedSquare {
+                self.highlightSprite = SKSpriteNode(imageNamed: highlightSpriteFileName)
+
+                // Set size of piece sprite
+                let highlightWidth = chessScene.size.width * chessScene.getPieceWidth()
+                let highlightHeight = chessScene.size.height * chessScene.getPieceHeight()
+                self.highlightSprite.size = CGSize(width: highlightWidth, height: highlightHeight)
+
+                // Set location of piece sprite
+                let xPos = chessScene.size.width * spriteScenePosition.x
+                let yPos = chessScene.size.height * spriteScenePosition.y
+                highlightSprite.position = CGPoint(x: xPos, y: yPos+5)
+                highlightSprite.zPosition = 1
+
+                chessScene.addChild(self.highlightSprite)
+            } else {
+                self.highlightSprite.removeFromParent()
+            }
+        }
+    }
+    
+    init(arrayRow: Int, arrayCol: Int, spriteScenePosition: CGPoint, xSquareOffset: CGFloat, ySquareOffset: CGFloat, inChessScene: ChessScene) {
+        self.row = arrayRow
+        self.col = arrayCol
+        self.spriteScenePosition = spriteScenePosition
+        self.xSquareOffset = xSquareOffset
+        self.ySquareOffset = ySquareOffset
+        self.chessScene = inChessScene
+        pieceSprite.isUserInteractionEnabled = false
+    }
+    
+    func setPiece(pieceSpriteFileName: String) {
+        
+        self.hasPiece = true
+        self.pieceSpriteFileName = pieceSpriteFileName
+        self.pieceSprite = SKSpriteNode(imageNamed: pieceSpriteFileName)
+        self.pieceSprite.name = pieceSpriteFileName
+        
+        // Set size of piece sprite
+        let pieceWidth = chessScene.size.width * chessScene.getPieceWidth()
+        let pieceHeight = chessScene.size.height * chessScene.getPieceHeight()
+        self.pieceSprite.size = CGSize(width: pieceWidth, height: pieceHeight)
+        
+        // Set location of piece sprite
+        let xPos = chessScene.size.width * spriteScenePosition.x
+        let yPos = chessScene.size.height * spriteScenePosition.y
+        pieceSprite.position = CGPoint(x: xPos, y: yPos)
+        pieceSprite.zPosition = 2
+        
+        chessScene.addChild(pieceSprite)
+    }
+    
+    func removePiece() {
+        self.hasPiece = false
+        self.pieceSprite.removeFromParent()
+        
+    }
+    
+    // Mark: Getters and Setters
+    func getSquareTopBoundsPosition() -> CGFloat {
+        return self.spriteScenePosition.y + (self.ySquareOffset / 2)
+    }
+    
+    func getSquareLeftBoundsPosition() -> CGFloat {
+        return self.spriteScenePosition.x - (self.xSquareOffset / 2)
+    }
 }
